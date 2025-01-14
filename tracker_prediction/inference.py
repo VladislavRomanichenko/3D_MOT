6806421c4136b6135ef34c4f31e25b3c523b2195
mark0.1 <mark0_1@example.com>
2025-01-14 20:36:16 +0300

    delete class TfConverter

--- tracker_prediction/inference.py
+++ tracker_prediction/inference.py
@@ -1,5 +1,9 @@
 import numpy
 import math
+from collections import namedtuple
+from packaging import version
+from scipy.spatial.transform import Rotation
+import scipy
 
 import rclpy
 import rclpy.clock
@@ -14,155 +18,134 @@ from tf2_ros.transform_listener import TransformListener
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy
 
-from objects_msgs.msg import ObjectArray, DynamicObjectArray
+from objects_msgs.msg import ObjectArray, DynamicObjectArray, DynamicObject
+from geometry_msgs.msg import Point, Pose, Quaternion, Transform, Vector3
+from sensor_msgs.msg import CameraInfo
 
-#from src import ObjectTfConverter
+# class ObjectTfConverter:
 
+#     def __init__(self, node):
+#         self._EPS = numpy.finfo(float).eps * 4.0
 
-#TODO: разобраться с cache_time в buffer, который инициализируется в конструкторе
-#      разобраться с математикой в quaternion_matrix, euler_from_matrix_vec, euler_to_matrix
+#         self.node = node
+#         self.fixed_frame = 'local_map'
 
-class ObjectTfConverter:
+#         self.buffer = Buffer()
 
-    def __init__(self, node):
-        self._EPS = numpy.finfo(float).eps * 4.0
-
-        self.node = node
-        self.fixed_frame = 'base_link'
-
-        self.buffer = Buffer()
-        self.buffer.clear()
-        self.listener = TransformListener(self.buffer, self.node)
-        self.prev_transform = None
+#         self.listener = TransformListener(self.buffer, self.node)
+#         self.prev_transform = None
 
 
-    def get_transform(self, frame_id, stamp):
-        try:
-            transform = self.buffer.lookup_transform(
-                self.fixed_frame, frame_id, stamp
-                )
+#     def quaternion_matrix(self, quaternion):
+#         q = numpy.array(quaternion[:4], dtype=numpy.float64, copy=True)
+#         nq = numpy.dot(q, q)
 
-            self.prev_transform = transform
-            return transform
+#         if nq < self._EPS:
+#             return numpy.identity(4)
 
-        #Обработка ошибок
-        except(
-            tf2_ros.LookupException,
-            tf2_ros.ConnectivityException,
-            tf2_ros.ExtrapolationException
-        ) as error:
-            self.node.get_logger().warn("TF wait-time limit reached, using previous tf")
-            self.node.get_logger().set_level(rclpy.logging.LoggingSeverity.DEBUG)
-            self.node.get_logger().warn(f"TF Error: {str(error)}, using previous tf")
-            return self.prev_transform #Используем предыдущую трансформацию
+#         q *= math.sqrt(2.0 / nq)
+#         q = numpy.outer(q, q)
+
+#         return numpy.array((
+#             (1.0-q[1, 1]-q[2, 2],     q[0, 1]-q[2, 3],     q[0, 2]+q[1, 3], 0.0),
+#             (    q[0, 1]+q[2, 3], 1.0-q[0, 0]-q[2, 2],     q[1, 2]-q[0, 3], 0.0),
+#             (    q[0, 2]-q[1, 3],     q[1, 2]+q[0, 3], 1.0-q[0, 0]-q[1, 1], 0.0),
+#             (                0.0,                 0.0,                 0.0, 1.0)
+#             ), dtype=numpy.float64)
 
 
-    def quaternion_matrix(self, quaternion):
-        q = numpy.array(quaternion[:4], dtype=numpy.float64, copy=True)
-        nq = numpy.dot(q, q)
+#     def euler_from_matrix_vec(self, matrix):
+#         if matrix.ndim == 2:
+#             matrix = matrix[numpy.newaxis, :, :]  # Добавляем новую ось, если матрица двумерная
+#         elif matrix.ndim != 3:
+#             raise ValueError("Матрица должна быть трехмерной (N, 3, 3)")
+#         pitch = numpy.arctan2(matrix[:, 2, 1], numpy.sqrt(numpy.power(matrix[:, 0, 0], 2) + numpy.power(matrix[:, 1, 0], 2)))
 
-        if nq < self._EPS:
-            return numpy.identity(4)
+#         deg_pos_90 = numpy.full(pitch.shape, 1.5707, dtype=numpy.float16)
+#         deg_neg_90 = numpy.full(pitch.shape, 1.5707, dtype=numpy.float16)
 
-        q *= math.sqrt(2.0 / nq)
-        q = numpy.outer(q, q)
+#         pos_90_mask = deg_pos_90 == pitch
+#         neg_90_mask = deg_neg_90 == pitch
 
-        return numpy.array((
-            (1.0-q[1, 1]-q[2, 2],     q[0, 1]-q[2, 3],     q[0, 2]+q[1, 3], 0.0),
-            (    q[0, 1]+q[2, 3], 1.0-q[0, 0]-q[2, 2],     q[1, 2]-q[0, 3], 0.0),
-            (    q[0, 2]-q[1, 3],     q[1, 2]+q[0, 3], 1.0-q[0, 0]-q[1, 1], 0.0),
-            (                0.0,                 0.0,                 0.0, 1.0)
-            ), dtype=numpy.float64)
+#         not_90 = numpy.logical_and(~pos_90_mask, ~neg_90_mask)
 
+#         yaw_not_90 = numpy.arctan2(matrix[:, 1, 0], matrix[:, 0, 0])
+#         yaw_pos_90 = numpy.arctan2(matrix[:, 1, 2], matrix[:, 0, 2])
+#         yaw_neg_90 = numpy.arctan2(-matrix[:, 1, 2], -matrix[:, 0, 2])
 
-    def euler_from_matrix_vec(self, matrix):
-        if matrix.ndim == 2:
-            matrix = matrix[numpy.newaxis, :, :]  
+#         yaw = numpy.zeros(pitch.shape, dtype=numpy.float16)
 
-        elif matrix.ndim != 3:
-            raise ValueError("Матрица должна быть трехмерной (N, 3, 3)")
-        
-        pitch = numpy.arctan2(matrix[:, 2, 1], numpy.sqrt(numpy.power(matrix[:, 0, 0], 2) + numpy.power(matrix[:, 1, 0], 2)))
+#         yaw[pos_90_mask] = yaw_pos_90[pos_90_mask]
+#         yaw[not_90] = yaw_not_90[not_90]
+#         yaw[neg_90_mask] = yaw_neg_90[neg_90_mask]
 
-        deg_pos_90 = numpy.full(pitch.shape, 1.5707, dtype=numpy.float16)
-        deg_neg_90 = numpy.full(pitch.shape, 1.5707, dtype=numpy.float16)
-
-        pos_90_mask = deg_pos_90 == pitch
-        neg_90_mask = deg_neg_90 == pitch
-
-        not_90 = numpy.logical_and(~pos_90_mask, ~neg_90_mask)
-
-        yaw_not_90 = numpy.arctan2(matrix[:, 1, 0], matrix[:, 0, 0])
-        yaw_pos_90 = numpy.arctan2(matrix[:, 1, 2], matrix[:, 0, 2])
-        yaw_neg_90 = numpy.arctan2(-matrix[:, 1, 2], -matrix[:, 0, 2])
-
-        yaw = numpy.zeros(pitch.shape, dtype=numpy.float16)
-
-        yaw[pos_90_mask] = yaw_pos_90[pos_90_mask]
-        yaw[not_90] = yaw_not_90[not_90]
-        yaw[neg_90_mask] = yaw_neg_90[neg_90_mask]
-
-        return yaw
+#         return yaw
 
 
-    def euler_to_matrix(self, angles, coords):
-        assert len(angles) == len(coords)
+#     def euler_to_matrix(self, angles, coords):
+#         assert len(angles) == len(coords)
 
-        num_samples = len(angles)
+#         num_samples = len(angles)
 
-        coss = numpy.cos(angles)
-        sins = numpy.sin(angles)
-        nels = numpy.zeros(num_samples, dtype=numpy.float16)
-        ones = numpy.ones(num_samples, dtype=numpy.float16)
+#         coss = numpy.cos(angles)
+#         sins = numpy.sin(angles)
+#         nels = numpy.zeros(num_samples, dtype=numpy.float16)
+#         ones = numpy.ones(num_samples, dtype=numpy.float16)
 
-        Rz = numpy.stack((
-            numpy.stack((coss, sins, nels)),
-            numpy.stack((-sins, coss, nels)),
-            numpy.stack((nels, nels, ones))
-        ), axis=0)  # Стек наших массивов по оси 0
+#         Rz = numpy.stack((
+#             numpy.stack((coss, sins, nels)),
+#             numpy.stack((-sins, coss, nels)),
+#             numpy.stack((nels, nels, ones))
+#         ), axis=0)  # Стек наших массивов по оси 0
 
-        return Rz
+#         return Rz
 
 
-    def transform_pose(self, box3d, header):
-        transform = self.get_transform(header.frame_id, header.stamp)
+#     def transform_pose(self, box3d, header):
+#         transform = self.get_transform(header.frame_id, header.stamp)
 
-        #Если нужен будет вывод центра робота
-        #robot_center = numpy.array([[0, 0, 0, 0]], dtype=numpy.float16) # X, Y, Z, yaw
+#         #Если нужен будет вывод центра робота
+#         #robot_center = numpy.array([[0, 0, 0, 0]], dtype=numpy.float16) # X, Y, Z, yaw
 
-        if transform is None:
-            self.node.get_logger().warn("No transform received! ")
-            return box3d
+#         if transform is None:
+#             self.node.get_logger().warn("No transform received! ")
+#             return box3d
 
-        #Матирца ротаций
-        rotation = numpy.array([
-            transform.transform.rotation.x,
-            transform.transform.rotation.y,
-            transform.transform.rotation.z,
-            transform.transform.rotation.w,
-        ], dtype=numpy.float16)
+#         #Матирца ротаций
+#         rotation = numpy.array([
+#             transform.transform.rotation.x,
+#             transform.transform.rotation.y,
+#             transform.transform.rotation.z,
+#             transform.transform.rotation.w,
+#         ], dtype=numpy.float16)
 
-        #Матрица трансляций
-        translation = numpy.array([
-            transform.transform.translation.x,
-            transform.transform.translation.y,
-            transform.transform.translation.z,
-        ], dtype=numpy.float16)
+#         #Матрица трансляций
+#         translation = numpy.array([
+#             transform.transform.translation.x,
+#             transform.transform.translation.y,
+#             transform.transform.translation.z,
+#         ], dtype=numpy.float16)
 
-        M = self.quaternion_matrix(rotation)
-        P = translation
+#         M = self.quaternion_matrix(rotation)
+#         P = translation
 
-        other_P = box3d[:, :3]
-        other_M = self.euler_to_matrix(box3d[:, -1], box3d[:, :3])
+#         other_P = box3d[:, :3]
+#         other_M = self.euler_to_matrix(box3d[:, -1], box3d[:, :3])
 
-        other_P = (M[:3, :3] @ other_P.T).T + P
-        other_M = M[:3, :3] @ other_M.astype(numpy.float32)
-        other_M = other_M.reshape(-1, 3, 3)
+#         other_P = (M[:3, :3] @ other_P.T).T + P
+#         other_M = M[:3, :3] @ other_M.astype(numpy.float32)
+#         other_M = other_M.reshape(-1, 3, 3)
+#         print(other_M.shape)
+#         box3d[:, -1] = self.euler_from_matrix_vec(other_M).astype(numpy.float16)
+#         box3d[:, :3] = other_P.astype(numpy.float16)
+#         return box3d
 
-        box3d[:, -1] = self.euler_from_matrix_vec(other_M).astype(numpy.float16)
-        box3d[:, :3] = other_P.astype(numpy.float16)
 
-        return box3d
+if version.parse(scipy.__version__) < version.parse('1.4'):
+    class RotationMod(Rotation):
+        def as_matrix(self):
+            return self.as_dcm()
+    Rotation = RotationMod
 
 
 class Tracker(Node):
@@ -186,69 +169,137 @@ class Tracker(Node):
         self.subscriber = self.create_subscription(ObjectArray, self.subscriber_topic, self.tracker_callback, qos)
         self.publisher = self.create_publisher(DynamicObjectArray, self.publisher_topic, qos)
 
-        self.object_tranformer = ObjectTfConverter(self)
+        #Create parameter for transformation
+        self.fixed_frame = self.declare_parameter('fixed_frame', 'local_map').value
+
+        self.queue_size = self.declare_parameter('queue_size', 5).value
+
+
+        self.buffer = Buffer()
+        self.listener = TransformListener(self.buffer, self)
+        self.prev_transform = None
+
         self.get_logger().info('Initializing is OK')
+
+    def get_transform(self, frame_id, stamp):
+        try:
+            transform = self.buffer.lookup_transform(
+                self.fixed_frame, frame_id, stamp
+                )
+            self.prev_transform = transform
+            return transform
+
+        except(
+            tf2_ros.LookupException,
+            tf2_ros.ConnectivityException,
+            tf2_ros.ExtrapolationException
+        ) as error:
+            self.node.get_logger().warn("TF wait-time limit reached, using previous tf")
+            self.node.get_logger().set_level(rclpy.logging.LoggingSeverity.DEBUG)
+            self.node.get_logger().warn(f"TF Error: {str(error)}, using previous tf")
+            return self.prev_transform #Используем предыдущую трансформацию
+
+
+    def Rt_from_tq(self, t, q):
+        Rt = numpy.eye(4)
+        Rt[:3, :3] = Rotation.from_quat((q.x, q.y, q.z, q.w)).as_matrix()
+        Rt[:3, 3] = t.x, t.y, t.z
+        return Rt
+
+
+    def Rt_from_Pose(self, pose):
+        return self.Rt_from_tq(pose.position, pose.orientation)
+
+
+    def Rt_from_Transform(self, tf):
+        return self.Rt_from_tq(tf.translation, tf.rotation)
+
+
+    def Pose_from_Rt(self, Rt):
+        pose = Pose()
+        q = pose.orientation
+        t = pose.position
+        q.x, q.y, q.z, q.w = Rotation.from_matrix(Rt[:3, :3]).as_quat()
+        t.x, t.y, t.z = Rt[:3, 3]
+        return pose
+
+
+    def transform_object(self, obj, tf):
+        obj.pose = self.Pose_from_Rt(
+            numpy.dot(self.Rt_from_Transform(tf), self.Rt_from_Pose(obj.pose)))
+
+
+    def set_object_yaw(self, obj, yaw, camera_frame=False):
+        if camera_frame:
+            R = numpy.array([
+                [-numpy.sin(yaw), -numpy.cos(yaw), 0.0],
+                [0.0, 0.0, -1.0],
+                [numpy.cos(yaw), -numpy.sin(yaw), 0.0],
+            ])
+            q = Rotation.from_matrix(R).as_quat()
+            obj.pose.orientation.x = q[0]
+            obj.pose.orientation.y = q[1]
+            obj.pose.orientation.z = q[2]
+            obj.pose.orientation.w = q[3]
+        else:
+            obj.pose.orientation.x = 0
+            obj.pose.orientation.y = 0
+            obj.pose.orientation.z = numpy.sin(yaw / 2)
+            obj.pose.orientation.w = numpy.cos(yaw / 2)
+
+
+    def get_object_yaw(self, obj, camera_frame=False):
+        if camera_frame:
+            q = obj.pose.orientation
+            R = Rotation.from_quat((q.x, q.y, q.z, q.w)).as_matrix()
+            yaw = numpy.arctan2(-R[0, 0], -R[0, 1])
+        else:
+            yaw = numpy.arctan2(obj.pose.orientation.z, obj.pose.orientation.w)
+        return yaw
+
+
+    def object_points(self, obj, Rt=None):
+        dx, dy, dz = obj.size.x / 2, obj.size.y / 2, obj.size.z / 2
+        # additional axis with ones for matrix multiplication
+        pts = numpy.array([
+            [-dx, -dy, -dz, 1],  # 0: back bottom right
+            [+dx, -dy, -dz, 1],  # 1: front bottom right
+            [+dx, +dy, -dz, 1],  # 2: front bottom left
+            [-dx, +dy, -dz, 1],  # 3: back bottom left
+            [-dx, -dy, +dz, 1],  # 4: back top right
+            [+dx, -dy, +dz, 1],  # 5: front top right
+            [+dx, +dy, +dz, 1],  # 6: front top left
+            [-dx, +dy, +dz, 1],  # 7: back top left
+        ])
+        if Rt is None:
+            Rt = numpy.eye(4)
+        res = numpy.dot(numpy.dot(Rt, self.Rt_from_Pose(obj.pose)), pts.T).T[:, :3]
+        return res
+
+
+    EDGES_INDICES = [(0, 1), (1, 2), (2, 3), (3, 0), (0, 4), (1, 5), (2, 6),
+                    (3, 7), (4, 5), (5, 6), (6, 7), (7, 4), (1, 6), (2, 5)]
 
 
     def tracker_callback(self, objects):
-        self.box3d = []
-        self.scores = []
-        self.label_preds = []
+        tf = self.get_transform(objects.header.frame_id, objects.header.stamp)
+
+        dynamic_objects = DynamicObjectArray()
+        dynamic_object = DynamicObject()
+
+        dynamic_objects.header = objects.header
 
         for obj in objects.objects:
-            orientation = obj.pose.orientation
+            self.transform_object(obj, tf)
+            dynamic_object.object = obj
+            dynamic_objects.append(dynamic_object)
 
-            x, y, z, w = orientation.x, orientation.y, orientation.z, orientation.w
-            self.yaw = numpy.arctan2(2.0 * (w * z + x * y), 1.0 - 2.0 * (y * y + z * z))
-            self.theta = -self.yaw - numpy.pi / 2
-
-            self.box3d.append([obj.pose.position.x,
-                                obj.pose.position.y,
-                                obj.pose.position.z,
-                                obj.size.y,
-                                obj.size.x,
-                                obj.size.z,
-                                self.theta])
-
-        self.box3d = numpy.array(self.box3d, dtype=numpy.float16)
-
-        if(len(self.box3d) > 0):
-            self.box3d[:, -1] = -self.box3d[:, -1] - numpy.pi / 2
-            self.box3d = self.object_tranformer.transform_pose(self.box3d, objects.header)
-
-        for i, obj in enumerate(objects.objects):
-            print(f"""
-            pose:
-                position:
-                x: {self.box3d[i][0]}
-                y: {self.box3d[i][1]}
-                z: {self.box3d[i][2]}
-                orientation:
-                x: {obj.pose.orientation.x}
-                y: {obj.pose.orientation.y}
-                z: {obj.pose.orientation.z}
-                w: {obj.pose.orientation.w}
-            size:
-                x: {self.box3d[i][4]}  # size.x
-                y: {self.box3d[i][3]}  # size.y
-                z: {self.box3d[i][5]}  # size.z
-            ---""")
+        self.publisher.publish(dynamic_objects)
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = Tracker()
-
-    # executor = rclpy.executors.MultiThreadedExecutor()
-    # executor.add_node(node)
-
-    # try:
-    #     executor.spin()
-    # except KeyboardInterrupt:
-    #     pass
-
-    # while rclpy.ok():
-    #     rclpy.spin_once(node)
 
     try:
         rclpy.spin(node)
